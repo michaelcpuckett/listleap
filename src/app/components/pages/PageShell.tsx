fff83b657f848da01966c9758ae287da5eb0675b
Michael Puckett <michaelcpuckett@gmail.com>
2023-11-21 09:41:44 -0500

    prevent safari text zooom

--- src/app/components/pages/PageShell.tsx
+++ src/app/components/pages/PageShell.tsx
@@ -22,7 +22,7 @@ export function PageShell(
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <script src="/head.js"></script>
         {cssUrls.map((url) => (
