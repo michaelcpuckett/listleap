80565535f4fe9baa15592f75823e32fe5b3b74d6
Michael Puckett <michaelcpuckett@gmail.com>
2023-11-29 13:11:35 -0500

    fixup focusable selector

--- src/client/elements/ViewContainerElement/constants.ts
+++ src/client/elements/ViewContainerElement/constants.ts
@@ -23,4 +23,4 @@ export function isHtmlElement(element: unknown): element is HTMLElement {
 }
 
 export const FOCUSABLE_ELEMENT_SELECTOR =
-  ':is(button, summary, [href], input, select, textarea, [tabindex]):not([tabindex="-1"]):not([type="hidden"]):not([disabled]):not([readonly])';
+  ':is(a, button, summary, [href], input, select, textarea, [tabindex]):not([tabindex="-1"]):not([type="hidden"]):not([disabled]):not([readonly])';
