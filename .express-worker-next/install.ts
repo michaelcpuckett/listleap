062aa9a582f3270cc752eca19ec564c671ed7e26
Michael Puckett <michaelcpuckett@gmail.com>
2025-01-13 10:13:23 -0500

    cleanup

--- .express-worker-next/install.ts
+++ .express-worker-next/install.ts
@@ -1,6 +1,6 @@
 declare var self: ServiceWorkerGlobalScope;
 
-import URLS_TO_CACHE from './static.json';
+import URLS_TO_CACHE from '@express-worker/next/static.json';
 
 export function handleInstall(event: Event) {
   if (!(event instanceof ExtendableEvent)) {
--- .express-worker-next/sw.ts
+++ .express-worker-next/sw.ts
@@ -1,3 +1,34 @@
-import useNextArchitecture from '@express-worker/next/NextArchitecture';
+declare var self: ServiceWorkerGlobalScope;
 
-useNextArchitecture();
+import { ExpressWorker } from '@express-worker/app';
+import useAppRouter from '@express-worker/next/AppRouter';
+import { FormDataMiddleware } from '@express-worker/next/FormDataMiddleware';
+import { handleInstall } from '@express-worker/next/install';
+import { QueryParamsMiddleware } from '@express-worker/next/QueryParamsMiddleware';
+import useStaticFiles from '@express-worker/next/StaticFiles';
+
+export default (function useNextArchitecture() {
+  // Populates the cache on install.
+  self.addEventListener('install', handleInstall);
+
+  // Immediately takes control of the page on activation.
+  self.addEventListener('activate', () => {
+    self.clients.claim();
+  });
+
+  const app = new ExpressWorker();
+
+  // Parses query params as `req.query`.
+  app.use(QueryParamsMiddleware);
+
+  // Parses form data as `req.data`.
+  app.use(FormDataMiddleware);
+
+  // Serve HTML pages via the App Router.
+  useAppRouter(app);
+
+  // Serve static files.
+  useStaticFiles(app);
+
+  return app;
+})();
