4585e03c9e44c0a004e83e25a81eecd30ead1b6c
Michael Puckett <michaelcpuckett@gmail.com>
2025-01-13 11:21:56 -0500

    styling tweaks

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -43,7 +43,8 @@ export default function HomePage({ initialNotes }: { initialNotes: Note[] }) {
 
   return (
     <main>
-      <h1>Notes</h1>
+      <h1>Notes App</h1>
+      <p>This is a simple notes app. Notes are saved locally (to IndexedDB).</p>
       <button
         className="button"
         onClick={addNote}
