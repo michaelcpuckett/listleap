752ed66ca09a875181e7996c37ac10a512da5747
Michael Puckett <michaelcpuckett@gmail.com>
2025-01-14 05:59:58 -0500

    fix middleware

--- .express-worker-app-router/Router.tsx
+++ .express-worker-app-router/Router.tsx
@@ -1,11 +1,22 @@
-import {
-  ExpressWorker,
-  ExpressWorkerRequest,
-  ExpressWorkerResponse,
-} from '@express-worker/app';
+import { ExpressWorker } from '@express-worker/app';
 import { PageShell } from '@express-worker/app-router/PageShell';
 import { renderToString } from 'react-dom/server';
 import Routes from './routes';
+
+interface FormDataWithArrayValue {
+  [key: `${string}[]`]: string[] | undefined;
+}
+
+interface FormDataWithStringValue {
+  [key: string]: string | undefined;
+}
+
+type NormalizedFormData = FormDataWithArrayValue & FormDataWithStringValue;
+
+interface AdditionalRequestProperties {
+  query: Record<string, string>;
+  data: NormalizedFormData;
+}
 
 function convertPath(path: string) {
   return path.replace(/\[([^\]]+)\]/g, ':$1');
@@ -24,26 +35,28 @@ export default function useAppRouter(app: ExpressWorker) {
   }>(Routes)) {
     app.get(
       convertPath(path),
-      async (req: ExpressWorkerRequest, res: ExpressWorkerResponse) => {
-        try {
-          const initialProps = getStaticProps
-            ? await getStaticProps(req.params)
-            : {};
+      ExpressWorker.applyAdditionalRequestProperties<AdditionalRequestProperties>(
+        async (req, res) => {
+          try {
+            const initialProps = getStaticProps
+              ? await getStaticProps(req.params)
+              : {};
 
-          const renderResult = renderToString(
-            <PageShell
-              {...(metadata || {})}
-              initialData={initialProps}
-            >
-              <Component {...initialProps} />
-            </PageShell>,
-          );
+            const renderResult = renderToString(
+              <PageShell
+                {...(metadata || {})}
+                initialData={initialProps}
+              >
+                <Component {...initialProps} />
+              </PageShell>,
+            );
 
-          res.send(renderResult);
-        } catch (error) {
-          res.status(404).send('Not found');
-        }
-      },
+            res.send(renderResult);
+          } catch (error) {
+            res.status(404).send('Not found');
+          }
+        },
+      ),
     );
   }
 }
